872f9edae4e4e077756ef6f523b4cdcc7c5a8c39
SergeyZu <vechniystudent19@yandex.ru>
2023-05-28 07:38:59 +0700

    вынес app.js из src

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./src/routes');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb')
+  .then(() => {
+    console.log('Успешное подключение к базе данных');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Ошибка подключения к базе данных ${err.name}`);
+  });
+
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: new mongoose.Types.ObjectId('646d6819a72ab810aca5fabb'),
+  };
+  next();
+});
+
+app.use(router);
