402325c2b74042caf5224bc80049f756710fdba3
SergeyZu <vechniystudent19@yandex.ru>
2023-06-11 00:13:07 +0700

    asdf

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const usersController = require('../controllers/users');
-const { validateUserId } = require('../middlewares/validate');
+const {
+  validateCreateUser,
+  validateUserId,
+} = require('../middlewares/validate');
 
 router.get('/', usersController.getUsers);
 
@@ -8,7 +11,7 @@ router.get('/me', validateUserId, usersController.getUserData);
 
 router.get('/:user_id', usersController.getUserById);
 
-router.patch('/me', usersController.updateUser);
+router.patch('/me', validateCreateUser, usersController.updateUser);
 
 router.patch('/me/avatar', usersController.updateUserAvatar);
 
