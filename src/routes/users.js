829996868fb277e9eb90311dd195888647a7131a
SergeyZu <vechniystudent19@yandex.ru>
2023-06-11 00:46:38 +0700

    gs5sy

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const usersController = require('../controllers/users');
 const {
-  validateCreateUser,
+  validateUpdateUser,
   validateUserId,
   validateUpdateUserAvatar,
 } = require('../middlewares/validate');
@@ -12,7 +12,7 @@ router.get('/me', validateUserId, usersController.getUserData);
 
 router.get('/:user_id', validateUserId, usersController.getUserById);
 
-router.patch('/me', validateCreateUser, usersController.updateUser);
+router.patch('/me', validateUpdateUser, usersController.updateUser);
 
 router.patch(
   '/me/avatar',
