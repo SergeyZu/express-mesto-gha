245bb6af08cb86c65c17b58e78d986c5e64e84d6
SergeyZu <vechniystudent19@yandex.ru>
2023-06-10 19:42:49 +0700

    тест

--- src/models/card.js
+++ src/models/card.js
@@ -1,8 +1,8 @@
 /* eslint-disable operator-linebreak */
 const mongoose = require('mongoose');
 
-const validate =
-  /^(https|http)?:\/\/(www.)?[^-_.\s](\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})?(:\d+)?(.+[#a-zA-Z/:0-9]{1,})?\.(.+[#a-zA-Z/:0-9]{1,})?$/i;
+// const validate =
+//   /^(https|http)?:\/\/(www.)?[^-_.\s](\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})?(:\d+)?(.+[#a-zA-Z/:0-9]{1,})?\.(.+[#a-zA-Z/:0-9]{1,})?$/i;
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -14,7 +14,7 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: true,
-    match: validate,
+    // match: validate,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
